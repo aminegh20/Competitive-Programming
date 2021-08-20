c6aceece97cefcff42cbedb30f9d836072e6863a
Amine Ghoussaini <aminegh2001@outlook.com>
2021-08-20 11:41:18 +0300

    Cleaned up implementation

---
+++ CSES/Range Queries/static_sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int n, q;
+  cin >> n >> q;
+  vector<long long> pr(n + 1);
+  for (int i = 1; i < n + 1; i++) {
+    cin >> pr[i];
+    pr[i] += pr[i - 1];
+  } 
+  while (q--) {
+    int L, R;
+    cin >> L >> R;
+    cout << pr[R] - pr[L - 1] << '\n';
+  }
+  return 0;
+}
