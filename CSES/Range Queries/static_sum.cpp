282326d704f378b125896f32f423cfd7f6c32994
Amine Ghoussaini <aminegh2001@outlook.com>
2021-08-20 11:51:13 +0300

    Code cleanup!

--- CSES/Range Queries/static_sum.cpp
+++ CSES/Range Queries/static_sum.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  int n, q;
-  cin >> n >> q;
+  int n, qq;
+  cin >> n >> qq;
   vector<long long> pr(n + 1);
   for (int i = 1; i < n + 1; i++) {
     cin >> pr[i];
     pr[i] += pr[i - 1];
   } 
-  while (q--) {
+  while (qq--) {
     int L, R;
     cin >> L >> R;
     cout << pr[R] - pr[L - 1] << '\n';
